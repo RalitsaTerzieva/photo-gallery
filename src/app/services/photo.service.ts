bffe3edb018aad9edc818c868aedcc66429e2b63
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-07-21 16:03:21 +0300

    Add delete functionality

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -110,6 +110,26 @@ export class PhotoService {
     reader.readAsDataURL(blob);
   });
 
+  public async deletePicture(photo: UserPhoto, position: number) {
+    // Remove this photo from the Photos reference data array
+    this.photos.splice(position, 1);
+
+    // Update photos array cache by overwriting the existing photo array
+    Preferences.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
+
+    // delete photo file from filesystem
+    const filename = photo.filepath
+      .substr(photo.filepath.lastIndexOf('/') + 1);
+
+    await Filesystem.deleteFile({
+      path: filename,
+      directory: Directory.Data
+    });
+  }
+
 }
 
 
