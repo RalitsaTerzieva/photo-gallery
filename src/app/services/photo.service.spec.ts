f07ba45efec2f21cde995292987e7c7df3ef0407
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-07-26 16:34:43 +0300

    Add unit test for hybrid case and configurations

--- src/app/services/photo.service.spec.ts
+++ src/app/services/photo.service.spec.ts
@@ -7,26 +7,26 @@ import { Platform } from '@ionic/angular';
 import { PhotoService } from './photo.service';
 
 const capacitorMock = {
-  convertFileSrc: (uri: string) => 'mocked_path/' + uri,
-};
-
-const filesystemMock = {
-  writeFile: async (options: any) => ({ uri: 'mocked_file_path/' + options.path }),
+  convertFileSrc: (uri: string) => 'mocked_file_path/' + uri,
 };
 
 const platformMock = {
   is: (platform: string) => platform === 'hybrid',
 };
 
+
 describe('PhotoService', () => {
   let service: PhotoService;
 
   beforeEach(() => {
+    spyOn(Filesystem, 'writeFile');
+    (Filesystem.writeFile as any)
+      .and.returnValue(Promise.resolve({ uri: 'mocked_file_path/test.jpeg', data: 'mocked_base64_data' }));
+
     TestBed.configureTestingModule({
       providers: [
         PhotoService,
         { provide: Capacitor, useValue: capacitorMock },
-        { provide: Filesystem, useValue: filesystemMock },
         { provide: Platform, useValue: platformMock },
       ],
     });
@@ -36,7 +36,7 @@ describe('PhotoService', () => {
   it('should return the correct filepath and webviewPath for non-hybrid platform', async () => {
     const photo: Photo = {
       path: '/test_path',
-      webPath: 'mocked_web_path',
+      webPath: 'mocked_file_path',
       format: 'jpeg',
       saved: true,
     };
@@ -46,6 +46,22 @@ describe('PhotoService', () => {
     const result = await service.savePicture(photo);
 
     expect(result.filepath).toContain('.jpeg');
-    expect(result.webviewPath).toBe('mocked_web_path');
+    expect(result.webviewPath).toBe('mocked_file_path');
+  });
+
+  it('should return the correct filepath and webviewPath for hybrid platform', async () => {
+    const photo: Photo = {
+      path: '/test_path',
+      webPath: 'mocked_file_path',
+      format: 'jpeg',
+      saved: true,
+    };
+
+    spyOn(service.platform, 'is').and.returnValue(true);
+
+    const result = await service.savePicture(photo);
+
+    expect(result.filepath).toContain('.jpeg');
+    expect(result.webviewPath).toContain('mocked_file_path/');
   });
 });
