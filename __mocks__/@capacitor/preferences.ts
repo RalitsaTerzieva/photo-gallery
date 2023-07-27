1509316dfd91793d4a7218704fa06ace2f1d294b
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-07-27 17:02:42 +0300

    Remove comments

--- __mocks__/@capacitor/preferences.ts
+++ __mocks__/@capacitor/preferences.ts
@@ -1,11 +1,7 @@
 export const Preferences = {
     get: async (options: any): Promise<any> => {
-        // Return a mock value for the 'value' property
-        // Modify this mock value according to your test requirements
         return { value: 'mocked_value' };
     },
     set: async (options: any) => {
-        // Mock implementation for the 'set' method
-        // You can customize this implementation for your tests
     },
 };
